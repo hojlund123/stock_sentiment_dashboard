5d3cca79eb5e0f640fc453cfb6c05715d13e7259
hojlund123 <zere0.mail@gmail.com>
2023-03-14 17:34:54 +0100

    plotly box attributes

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def plot_hourly_sentiment(parsed_and_scored_news, ticker):
     mean_scores = parsed_and_scored_news.resample('H').mean()
 
     # Plot a bar chart with plotly
-    fig = px.bar(mean_scores, x=mean_scores.index, y='sentiment_score', title = ticker + ' Hourly Sentiment Scores')
+    fig = px.box(mean_scores, x=mean_scores.index, y='sentiment_score', title = ticker + ' Hourly Sentiment Scores')
     return fig # instead of using fig.show(), we return fig and turn it into a graphjson object for displaying in web page later
 
 def plot_daily_sentiment(parsed_and_scored_news, ticker):
@@ -97,7 +97,7 @@ def plot_daily_sentiment(parsed_and_scored_news, ticker):
     mean_scores = parsed_and_scored_news.resample('D').mean()
 
     # Plot a bar chart with plotly
-    fig = px.bar(mean_scores, x=mean_scores.index, y='sentiment_score', title = ticker + ' Daily Sentiment Scores')
+    fig = px.box(mean_scores, x=mean_scores.index, y='sentiment_score', title = ticker + ' Daily Sentiment Scores')
     return fig # instead of using fig.show(), we return fig and turn it into a graphjson object for displaying in web page later
 
 app = Flask(__name__)
